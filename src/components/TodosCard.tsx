b97d6a5e84098ebedd4636f7ec092dbce651bc90
Daniel Toba <danieloloruntoba681@gmail.com>
2022-06-26 23:50:43 +0100

    Feat: Working on edit feature which breaks the ui

--- src/components/TodosCard.tsx
+++ src/components/TodosCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Todo } from "../model";
 import "./card.css";
 
@@ -9,8 +9,11 @@ interface Props {
 }
 
 const TodosCard = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
   const handleDelete = (id: number) => {
     console.log(id);
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
   const handleDone = (id: number) => {
     setTodos(
@@ -24,21 +27,65 @@ const TodosCard = ({ todo, todos, setTodos }: Props) => {
     <div>
       <div className="card">
         <li className="todos">
-          {todo.todo}
-          <span style={{ marginLeft: 10 }}>
-            <button
-              className="delete__button"
-              onClick={() => handleDelete(todo.id)}
-            >
-              x
-            </button>
-            <button
-              className="tick__button"
-              onClick={() => handleDone(todo.id)}
-            >
-              Done
-            </button>
-          </span>
+          {todo.isDone ? (
+            <>
+              <br />
+              <s>{todo.todo}</s>
+              <span style={{ marginLeft: 10 }}>
+                <button
+                  className="delete__button"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  x
+                </button>
+                <button
+                  className="tick__button"
+                  onClick={() => handleDone(todo.id)}
+                >
+                  {todo.isDone ? "Undone" : "Done"}
+                </button>
+                <button
+                  className="edit__button"
+                  onClick={() => {
+                    // if (!edit && !todo.isDone) {
+                    setEdit(!edit);
+                    // }
+                  }}
+                >
+                  Edit
+                </button>
+              </span>
+            </>
+          ) : (
+            <>
+              {todo.todo}
+              <span style={{ marginLeft: 10 }}>
+                <button
+                  className="delete__button"
+                  onClick={() => handleDelete(todo.id)}
+                >
+                  x
+                </button>
+                <button
+                  className="tick__button"
+                  onClick={() => handleDone(todo.id)}
+                >
+                  Done
+                </button>
+                <button
+                  className="edit__button"
+                  onClick={() => {
+                    // if (!edit && !todo.isDone) {
+                    setEdit(!edit);
+                    // }
+                  }}
+                >
+                  Edit
+                </button>
+              </span>
+            </>
+          )}
+          {edit && <input type="text" />}
         </li>
       </div>
     </div>
