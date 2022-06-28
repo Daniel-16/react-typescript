e9afa38b185ffe2ab4c17b14f3bae18141a3eedf
Daniel Toba <danieloloruntoba681@gmail.com>
2022-06-28 22:55:49 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import React, { useState } from "react";
-import Input from "./components/Input";
-import TodoLists from "./components/TodoLists";
-import { Todo } from "./model";
+import React from "react";
 
 const App: React.FC = () => {
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
   return (
     <div className="App mt-3">
-      <h1 style={{ textAlign: "center" }}>Task App</h1>
-      <Input todo={todo} setTodo={setTodo} todos={todos} setTodos={setTodos} />
-      <TodoLists todos={todos} setTodos={setTodos} />
+      <h1 style={{ textAlign: "center" }}>Water Drinking App</h1>
     </div>
   );
 };
